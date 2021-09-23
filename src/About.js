1597948f89a4d4bb2e5e1d715f8825970ecbab2f
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2021-09-24 00:45:19 +0100

    More stuff

--- src/About.js
+++ src/About.js
@@ -1,5 +1,10 @@
 const About = () => {
-    return (<h2>To do on about page...</h2>);
+    return (
+        <div>
+            <h2>Nothing here yet</h2>
+            <img className="chuck" alt="Chuck Norris" src="https://purepng.com/public/uploads/large/purepng.com-chuck-norrischuck-norrischucknorriscarlos-ray-norrisamerican-martial-artistactorproducerscreenwriter-1701528022186rjflr.png" />
+        </div>
+    );
 };
 
 export default About;
--- src/FetchNorrisJoke.js
+++ src/FetchNorrisJoke.js
@@ -4,17 +4,19 @@ import GetNewJoke from "./GetNewJoke";
 
 const FetchNorrisJoke = () => {
     const [joke, setJoke] = useState("No joke found");
+    const [loading, setLoading] = useState(true);
 
-    useEffect(() => fetchJokeFromAPI(), []);
+    useEffect(() => fetchJokeFromAPI(), [loading]);
 
     const fetchJokeFromAPI = async () => {
         const fetchAPI = await axios.get("https://api.chucknorris.io/jokes/random").then((response) => response?.data?.value?.trim() || "No joke found");
         setJoke(fetchAPI);
+        setLoading(false);
     };
 
     return (
         <div className="joke-container">
-            <p>{ joke }</p>
+            <p>{ loading ? "Loading joke..." : joke }</p>
             <GetNewJoke fetchJokeFromAPI={ fetchJokeFromAPI }/>
         </div>
     );
