cd32b25c65b03fee37c40b1c2fae614b7f8727a4
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2021-09-23 20:47:41 +0100

    Update PageTitle.js

--- src/PageTitle.js
+++ src/PageTitle.js
@@ -2,7 +2,7 @@ const PageTitle = () => {
     const pageTitle = "Random Chuck Norris Jokes";
     document.title = pageTitle;
 
-    return (<h1>{pageTitle }</h1>);
+    return (<h1>{ pageTitle }</h1>);
 };
 
 export default PageTitle;
